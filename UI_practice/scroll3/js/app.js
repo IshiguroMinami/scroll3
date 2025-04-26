a47c6483357970ca04c3f58662ff9d1b67676417
石黒みなみ <ishigurominami142@gmail.com>
2025-04-27 00:01:45 +0900

    first commit

---
+++ UI_practice/scroll3/js/app.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  function checkSections() {
+    var triggerBottom = $(window).height() * 0.6;
+
+    $('.section').each(function () {
+      var top = $(this).offset().top - $(window).scrollTop();
+
+      if (top < triggerBottom && top > -$(window).height() / 2) {
+        $(this).addClass('active');
+      } else {
+        $(this).removeClass('active');
+      }
+    });
+  }
+
+  $(window).on('scroll load', checkSections);
+
+  // ▼ contentにカーソルが入ったら .hover を付与（テキストにも反映）
+  $('.content').on('mouseenter', function () {
+    $(this).addClass('hover');
+  }).on('mouseleave', function () {
+    $(this).removeClass('hover');
+  });
+});
